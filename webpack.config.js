91fd9767a5642342ebae4d44d4ade84b947d71de
Werykowiak <werykowiak@gmail.com>
2021-08-04 14:11:54 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import css from "src/style.css";
 
 module.exports = {
   entry: './src/index.js',
