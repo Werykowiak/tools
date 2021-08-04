fa9c74219cee7ed2b056656ebffdcc89e055f2e8
Werykowiak <werykowiak@gmail.com>
2021-08-04 13:14:53 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-import html from "./file.html";
 
 module.exports = {
   entry: './src/index.js',
@@ -8,14 +7,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
