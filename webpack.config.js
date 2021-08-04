bc688511d74f019ff78071f86117717589fdfdba
Werykowiak <werykowiak@gmail.com>
2021-08-04 14:01:34 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
