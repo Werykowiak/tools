1cf9ceecced34b29fb453e4550bfd826a387144a
Werykowiak <werykowiak@gmail.com>
2021-08-05 11:05:27 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,16 +21,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(scss|sass)/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
+        test: /\.(s[ac]ss)/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
