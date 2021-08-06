06d029e4aea99b3ed5497057b3b638e8371ada5e
Werykowiak <werykowiak@gmail.com>
2021-08-06 09:18:28 +0200

    adding bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,15 @@ module.exports = {
       {
         test: /\.(s[ac]ss)/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
       },
     ],
   },
