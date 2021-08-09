32b5e84b61cdc4eb1bc474370efb5205a13b95bc
Werykowiak <werykowiak@gmail.com>
2021-08-09 15:00:54 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: ["file-loader"],
+        loaders: "file-loader",
         options: {
           name: './assets/pic.png',
         },
