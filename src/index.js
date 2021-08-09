2717e8650affff639b46de87bf9e340bd837c7a5
Werykowiak <werykowiak@gmail.com>
2021-08-09 14:16:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import _ from 'lodash';
 import "./style.scss";
 import mainImage from 'assets/pic.png';
 
-img.src = mainImage;
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
+
+    const mainImage = new Image();
+    mainImage.src = Icon;
+
+    element.appendChild(mainImage);
   
     return element;
   }
