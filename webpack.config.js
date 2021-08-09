cf502b23cc8736bf64e79a4ca64abb1d1a0e1b55
Werykowiak <werykowiak@gmail.com>
2021-08-09 14:46:18 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
