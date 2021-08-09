ff1050b8fa6a7096f233d8f6b9774b2e06af2e06
Werykowiak <werykowiak@gmail.com>
2021-08-09 14:07:38 +0200

    adding plugins

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         test: /\.(s[ac]ss)/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   devServer: {
